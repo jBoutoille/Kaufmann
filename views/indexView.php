1e29f36e57dbe8a022b87fda95952349c30e0c9e
Julien <julien.boutoille@gmail.com>
2019-09-11 11:18:52 +0200

    modif

--- views/indexView.php
+++ views/indexView.php
@@ -2,7 +2,15 @@
 
 
     <!-- HTML DE LA PAGE INDEX -->
-    <p> test index </p>
+
+    <div class="barOne"></div>
+    <div class="barTwo"></div>
+    <h1 id="sarl"> S.A.R.L 
+    	<br id="kaufmann"> G.KAUFMANN 
+    	<br id="text1"> Expertise comptable - Audit 
+    	<br id="text2"> Conseil & Accompagement
+    </h1>
+
 
 
 <?php
