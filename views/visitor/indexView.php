b49765f9f3e97c7f3bb9a9e67562194786705ca9
Julien <julien.boutoille@gmail.com>
2019-09-14 19:09:56 +0200

    modif du week-end julien

--- views/visitor/indexView.php
+++ views/visitor/indexView.php
@@ -4,7 +4,7 @@
     <!-- HTML DE LA PAGE INDEX -->
 <header>
 	<div id="imgHeader">
-	 <div id="filter">
+	 <div class="filterOne">
       <div id="titleHeader"> 
     	<p class="sarl">S.A.R.L</p>
     	<p id="kaufmann">G.KAUFMANN</p>
@@ -16,7 +16,7 @@
 </header>
 
     <!-- CABINET SECTION -->
-	<div>
+	<section>
 		<h2 class="titleNav">| Le cabinet</h2>
 			<p id="texteSectionOne">
 				On sait depuis longtemps que travailler avec du texte lisible et contenant du sens 
@@ -28,8 +28,8 @@
 				est source de distractions, et empêche de se concentrer sur la mise en page elle-même. 
 				L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.'
 			</p>
-	</div>
-	<div id="bluBoxOne">
+	</section>
+	<section id="bluBoxOne">
 		<h2 id="titleOne">Notre expertise en quelques chiffres :</h2>
 		<div id="barOne"></div>
 		<p id="texteSectionTwo">
@@ -48,76 +48,108 @@
 				<h2>Années d'expertise</h2>
 			</div>
 		</div>
-	</div>
+	</section>
 
 	<!-- SERVICES SECTION -->
-	<h2 class="titleNav">| Nos services</h2>	
-	<div id="serviceBox">
-		
-		<h2 class="serviceSectionOne">AUDIT</h2>
-		
-			<p>Nous intervenons au titre de vos besoins en matière de:</p>
-				
-				<ul>
-					<li> Commissairiat aux comptes.</li>
-					<li> Audit Contractuel.</li>
-					<li> Commissariat à la transformation.</li>
-					<li> Commissariat à la fusion.</li>
-					<li> Audit d'acquisition.</li>
-				</ul>
-		
-		<h2 class="serviceSectionTwo">EXPERTISE COMPTABLE</h2>
-
-			<p>Nous vous proposons un accompagnement sur mesure en comptabilité, 
-			fiscalité, social et juridique.</p>
-		
+	<section id="globalBoxService">	
+		<h2 class="titleNav">| Nos services</h2>	
+		<div id="serviceBox">
+			
+			<h2 class="serviceSection">AUDIT</h2>
+			
+				<p>Nous intervenons au titre de vos besoins en matière de :</p>
+					
 					<ul>
-						<li>Professions libérales (déclaration fiscale annuelles, conseil patrimonial, assistance fiscale, paie...)</li>
-						<li>Associations (tenue, accompagnement, révision...)</li>
-						<li>Particuliers (Impot sur le revenu, IFI, conseil patrimonial...)</li>
-						<li>Entreprises (tenue complète, comptabilité partagée, révision, secrétariat, paie...)</li>
+						<li> Commissairiat aux comptes.</li>
+						<li> Audit Contractuel.</li>
+						<li> Commissariat à la transformation.</li>
+						<li> Commissariat à la fusion.</li>
+						<li> Audit d'acquisition.</li>
 					</ul>
+			
+			<h2 class="serviceSection">EXPERTISE COMPTABLE</h2>
+
+				<p>Nous vous proposons un accompagnement sur mesure en comptabilité, 
+				fiscalité, social et juridique :</p>
+			
+						<ul>
+							<li>Professions libérales (déclaration fiscale annuelles, conseil patrimonial, assistance fiscale, paie...)</li>
+							<li>Associations (tenue, accompagnement, révision...)</li>
+							<li>Particuliers (Impot sur le revenu, IFI, conseil patrimonial...)</li>
+							<li>Entreprises (tenue complète, comptabilité partagée, révision, secrétariat, paie...)</li>
+						</ul>
 
 
-			<h2 class="serviceSectionTree">CONSEILS / ACCOMPAGNEMENT</h2>
-		
-					<ul>
-						<li>Création / reprise de socités.</li>
-						<li>Mise en place de tableaux de boards.</li>
-						<li>Prévisionnels.</li>
-						<li>Évaluation de socités.</li>
-						<li>Mise à disposition de logiciels sans engagement à tarifs avantageux:
-							"comptabilité, facturation, notes de frais"</li>
+			<h2 class="serviceSection">CONSEILS / ACCOMPAGNEMENT</h2>
+
+				<p>text à définir :</p>
+			
+						<ul>
+							<li>Création / reprise de socités.</li>
+							<li>Mise en place de tableaux de boards.</li>
+							<li>Prévisionnels.</li>
+							<li>Évaluation de socités.</li>
+							<li>Mise à disposition de logiciels sans engagement à tarifs avantageux:
+								"comptabilité, facturation, notes de frais"</li>
+						</ul>
+		</div>
+	</section>
+		<!-- CONTACT SECTION -->
+		<section id="bluBoxTwo">
+			<div class="filterTwo">
+
+			<h2 class="titleNav" id="contact">| Contact</h2>
+
+				<div id="boxContner">
+					<ul id="boxAdresse">
+						<h3 id="coordonnees">Coordonnées :</h3>
+
+							<li id="adresse">5 Place d'Angleterre <br>62200 BOULOGNE SUR MER</li>
+							<li id="telMobile">03 21 30 19 19</li>
+							<li id="adMail">contact@kaufman.fr</li>
+							<li id="numFax">03 91 18 65 65</li>
+
 					</ul>
-	</div>
+					<div id="twoBox">
+						<div id="boxContactOne">
+							<p class="textBtn"> Optenir un devis de comptable et d'experts-comptables</p>
+							<button class="myBtn">REMPLIR LE DEVIS</button>
+						</div>
+						<div id="boxContactTwo">
+							<p class="textBtn"> Rejoindre notre équipe</p>
+							<button class="myBtn">ENVOYER UN C.V</button>	
+						</div>
+					</div>
 
-	<!-- CONTACT SECTION -->
-	<div id="bluBoxTwo" >
-		<h2 class="titleNav" id="contact">| Contact</h2>
-		<div id="boxContner">
-			<ul id="boxAdresse">
-				<h3 id="coordonnees">Coordonnées :</h3>
-				<li id="adresse">5 Place d'Angleterre <br>62200 BOULOGNE SUR MER</li>
-				<li id="telMobile">03 21 30 19 19</li>
-				<li id="adMail">contact@kaufman.fr</li>
-				<li id="numFax">03 91 18 65 65</li>
-			</ul>
-			<div id="twoBox">
-				<div id="boxContactOne">
-					<p> Optenez un devis de comptable et d'experts-comptables</p>
-					<button class="myBtn">Remplir le devis</button>
-				</div>
-				<div id="boxContactTwo">
-					<p> Rejoindre notre équipe</p>
-					<button class="myBtn">Envoyer un c.v</button>	
 				</div>
 			</div>
+		</section>
+		<!-- ACTUALITES SECTION -->
+		<div id="globalNewsSection">
+			<h2 class="titleNav">| Actualités</h2>
 		</div>
-	</div>
-	<!-- ACTUALITES SECTION -->
-	<div class="titleNav">
-		<h2>| Actualités</h2>
-	</div>
+<!-- NEWSLETTER SECTION -->
+
+           <section class="newsletter">
+
+                  <div class="container">
+
+                    <div class="news-data">
+                    <div class="section-subheading">
+                          <h1 class="wow fadeInUp">Recevoir notre newsletter</h1>
+                    </div>
+
+                    <div class="input-group wow fadeInUp">
+                          <input type="email" class="form-control" placeholder="Entrez votre Email">
+                          <span class="input-group-btn">
+                                <button class="btn" type="submit">S'abonner</button>
+                          </span>
+                    </div>
+                    </div>
+
+                  </div>
+
+           </section>
 
 <?php
     $htmlTitle = 'Accueil | Cabinet G.KAUFMANN';
