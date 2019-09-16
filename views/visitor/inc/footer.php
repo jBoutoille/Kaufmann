d64ad562bb56867cf476838bac60afa5b141a2e8
Julien <julien.boutoille@gmail.com>
2019-09-16 14:20:44 +0200

    css

--- views/visitor/inc/footer.php
+++ views/visitor/inc/footer.php
@@ -5,28 +5,18 @@
               <ul id="adresseBox">
                 <li id="adresse">5 Place d'Angleterre <br>62200 BOULOGNE SUR MER</li>
                 <li id="telMobile">03 21 30 19 19</li>
-                <li id="adMail">contact@kaufman.fr</li>
+                <li id="adMail">contact@kaufmann.fr</li>
                 <li id="numFax">03 91 18 65 65</li>
+                <li id="linkedin">Linkedin</li>
               </ul>
 
               <div id="myLogo"></div>
 
             <div id="copyright">
-              <div>
                 <p>
-                  S.A.R.L au capital de 10.000€ - siret 429 453 392 00012 - code APE 6920 Z - N° de T.V.A INTRACOMMUNAUTAIRE : FR 77429 453 392    
+                  S.A.R.L au capital de 10.000€ - siret 429 453 392 00012 - code APE 6920 Z - N° de T.V.A INTRACOMMUNAUTAIRE : FR 77429 453 392 <br> © 2019 JBoutoille - JFasquelle Mention légales  
                 </p>
-              </div>
-
-              <div>
-                <p id="copyright">
-                  © 2019 JBoutoille - JFasquelle Mention légales
-                </p>
-
-                <div id="pageAdmin"></div>
-
-              </div>
-            </div> 
+            </div>
 
            </section>
 
