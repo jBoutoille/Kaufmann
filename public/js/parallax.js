9f19f8ae87ca79911244c8acafa6e1e29d08c14c
Julien <julien.boutoille@gmail.com>
2019-09-23 19:31:32 +0200

    modif

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -1,10 +1,10 @@
 
 	  var image = document.getElementById('imgHeader');
       new simpleParallax(image, {
-            scale: 1.7
+            scale: 1.8
       });
 
       	  var image = document.getElementById('imgBoxOne');
       new simpleParallax(image, {
-            scale: 1.3
+            scale: 1.4
       });
