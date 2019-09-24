d97a6be19999665cd6f301aa5e3730f2a2196265
Jérôme FASQUELLE <jf.jeromefasquelle@gmail.com>
2019-09-24 10:49:03 +0200

    Début récup des donneés + scripts nav php

--- controllers/dataController.php
+++ controllers/dataController.php
@@ -12,47 +12,47 @@
 
         // Formulaire de configuration du site
         elseif(isset($_POST['formAdminConfig1'])){
-            // Fonction dans FormManager qui update ça    
+            
         }
 
         // Formulaire de configuration des coordonnées
         elseif(isset($_POST['formAdminConfig2'])){
-            // Fonction dans FormManager qui update ça    
+  
         }
 
         // Formulaire d'édition de la section HEADER
         elseif(isset($_POST['formAdminEditHeader'])){
-            // Fonction dans FormManager qui update ça    
+ 
         }
 
         // Formulaire d'édition de la section LE CABINET
         elseif(isset($_POST['formAdminEditLecabinet'])){
-            // Fonction dans FormManager qui update ça    
+  
         }
 
         // Formulaire d'édition de la section EXPERTISE
         elseif(isset($_POST['formAdminEditExpertise'])){
-            // Fonction dans FormManager qui update ça    
+ 
         }
 
         // Formulaire d'édition de la section SERVICES
         elseif(isset($_POST['formAdminEditServices'])){
-            // Fonction dans FormManager qui update ça    
+
         }
 
         // Formulaire d'édition de la section CONTACT
         elseif(isset($_POST['formAdminEditContact'])){
-            // Fonction dans FormManager qui update ça    
+
         }
 
         // Formulaire d'édition de la section NEWSLETTER
         elseif(isset($_POST['formAdminEditNewsletter'])){
-            // Fonction dans FormManager qui update ça    
+ 
         }
 
         // Formulaire d'édition de la section FOOTER
         elseif(isset($_POST['formAdminEditFooter'])){
-            // Fonction dans FormManager qui update ça    
+  
         }
 
         // AUCUN FORMULAIRE
@@ -68,5 +68,11 @@
     else{
         $_TOKEN = NULL;
     }
+
+    // RECUPERATION DES TABLES
+    $infos = new InfosManager();
+    $RConfig = $infos->recupConfig();
+
+
 
 ?>
--- models/infosManager.php
+++ models/infosManager.php
@@ -2,13 +2,29 @@
     // classe de gestion des informations du site
 
     //// -> PERMET D'UPDATER TOUTES LES INFOS DU SITE DANS LA BASE
-    //// -> PERMET DE RECUPERER TOUTES LES INFOS DE LA BASE DANS DES VARIABLEs
+    //// -> PERMET DE RECUPERER TOUTES LES INFOS DE LA BASE DANS DES VARIABLES
 
-    class infosManager extends adminManager {
+    class InfosManager extends AdminManager {
 
-        public function updateConfig(){
-            parent::dbConnect();
+        // Fonction de connection à la BDD
+        public function dbC(){
+            try{
+                $db = new PDO('mysql:host=localhost;dbname=kaufmann;charset=utf8', 'root', 'root');
+                return $db;
+            }catch (Exception $e){
+                throw new Exception('Connexion SQL impossible');
+            }
+        }
+
+        public function recupConfig(){
+            $test = 1;
+            $bdd = $this->dbC();
+            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $query = $bdd->query("SELECT * FROM gkconfig");
+            $data = $query->fetch();
+            return $data;
+
         }
 
     }
-?> 
+?>
