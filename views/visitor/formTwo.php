38a7e77a3189a096866e77bf4900e80682d99aee
Jérôme FASQUELLE <jf.jeromefasquelle@gmail.com>
2019-10-01 14:50:04 +0200

    Beaucoup de modifications en back + formulaires OK

--- views/visitor/formTwo.php
+++ views/visitor/formTwo.php
@@ -9,34 +9,38 @@
 	</div>
 	<div class="containerText">
 		<p class="blockText foo-2">Envoyez-nous votre CV et Lettre de motivation en remplissant le formulaire ci-dessous</p>
-		<form action="./?treatment" method="POST">
+		<form action="./?treatment" method="POST" enctype="multipart/form-data">
 			<div class="inputData">
 				<label class="foo-3">Nom :</label>
-				<input type="text" class="form-control wow fadeInUp foo-3" name="visitorCandidatureNom">
+				<input type="text" class="form-control wow fadeInUp foo-3" name="visitorCandidatureNom" required>
 			</div>
 			<div class="inputData">
 				<label class="foo-3">Prénom :</label>
-				<input type="text" class="form-control wow fadeInUp foo-3" name="visitorCandidaturePrenom">
+				<input type="text" class="form-control wow fadeInUp foo-3" name="visitorCandidaturePrenom" required>
+			</div>
+			<div class="inputData">
+				<label class="foo-3">Adresse e-mail :</label>
+				<input type="text" class="form-control wow fadeInUp foo-3" name="visitorCandidatureMail" required>
+			</div>
+			<div class="inputData">
+				<label class="foo-3">Téléphone :</label>
+				<input type="number" class="form-control wow fadeInUp foo-3" name="visitorCandidatureTelephone" required>
 			</div>
 			<div class="inputData">
 				<label class="foo-3">Poste recherché :</label>
-				<input type="text" class="form-control wow fadeInUp foo-3" name="visitorCandidaturePoste">
+				<input type="text" class="form-control wow fadeInUp foo-3" name="visitorCandidaturePoste" required>
 			</div>
 			<div class="inputData">
 				<label class="foo-3">Message :</label>
-				<textarea rows="3" class="form-control wow fadeInUp foo-3" name="visitorCandidatureMessage"></textarea>
+				<textarea rows="3" class="form-control wow fadeInUp foo-3" name="visitorCandidatureMessage" required></textarea>
 			</div>
-			<label class="foo-3">Cv</label>
+			<label class="foo-3" for="fileOne">CV (10Mo max)</label>
 			<div class="custom-file">
-				<input type="file" class="custom-file-input foo3" id="validatedCustomFile" name="visitorCandidatureFile1" required>
-				<label class="custom-file-label foo-3" for="validatedCustomFile">Choisir un fichier</label>
-				<div class="invalid-feedback foo-3">Fichier non valide</div>
+				<input class="foo-3" type="file" name="fileOne" id="fileOne" value="10000000" required>
 			</div>
-			<label class="foo-3">Lettre de motivation</label>
+			<label class="foo-3" for="fileTwo">Lettre de motivation (10Mo max)</label>
 			<div class="custom-file">
-				<input type="file" class="custom-file-input" id="validatedCustomFile" name="visitorCandidatureFile2" required>
-				<label class="custom-file-label foo-3" for="validatedCustomFile">Choisir un fichier</label>
-				<div class="invalid-feedback foo-3">Fichier non valide</div>
+				<input class="foo-3" type="file" name="fileTwo" id="fileTwo" value="10000000" required>
 			</div>
 			<div class="add-prod wow fadeInUp foo-3">
 				<button type="submit" name="formVisitorCandidature" class="foo-3">ENVOYER</button>
